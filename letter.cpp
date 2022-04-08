8433365eb7efbcb03ec2cd40e383cd0e94302b1d
NotFiege <91145413+NotFiege@users.noreply.github.com>
2022-04-08 10:21:25 -0500

    Create letter.cpp

---
+++ letter.cpp
@@ -0,0 +1,7 @@
+int main() 
+{
+  std::cout << "Dear Self,\n";
+  std::cout << "If you're reading this later on, it means that you've made it. It was difficult but being consistent got you here.\n";
+  std::cout << "Keep coding, don't quit, and good things will come.\n";
+  std::cout << "Eddie, 4/4/2022\n";
+}
