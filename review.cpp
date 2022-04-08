23dd00c2896da84da58f4c7143761061383f81ec
NotFiege <91145413+NotFiege@users.noreply.github.com>
2022-04-08 10:23:27 -0500

    Create review.cpp

---
+++ review.cpp
@@ -0,0 +1,14 @@
+# Create a program that asks for distance in miles as input. The program will then output how much that distance is in kilometers.
+
+int main() {
+  // Add your code below  
+  std::cout << "Enter distance in miles: ";
+  std::cin >> distance_miles;
+
+  distance_kilometers;
+
+  distance_kilometers = (0.62137 * distance_miles);
+
+  std::cout << "The distance in kilometers is " << distance_miles << "\n";
+  return distance_miles
+}
