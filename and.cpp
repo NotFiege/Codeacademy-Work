3bdaccdec5331c38808d39b7e0f5acfe64cc1407
NotFiege <91145413+NotFiege@users.noreply.github.com>
2022-04-13 09:43:17 -0500

    Create and.cpp

---
+++ and.cpp
@@ -0,0 +1,12 @@
+// If hunger is true, and anger is true, then print out the word "Hangry".
+
+#include <iostream>
+
+int main() {
+  
+  int hunger = true;
+  int anger = true;
+
+  if (hunger == true && anger == true);
+    std::cout << "Hangry\n";
+}
