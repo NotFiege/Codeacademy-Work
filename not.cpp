acadc7cb2470432bfaf285e8085d98f79ffde05a
NotFiege <91145413+NotFiege@users.noreply.github.com>
2022-04-13 09:55:57 -0500

    Create not.cpp

---
+++ not.cpp
@@ -0,0 +1,12 @@
+// If the user is !logged_in, then print the phrase "Try again".
+
+#include <iostream>
+
+int main() {
+  
+  bool logged_in = false;
+
+  if (!logged_in){
+    std::cout << "Try again\n";
+  }
+}
